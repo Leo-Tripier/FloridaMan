42a9e4f17313d8b4bbad05e45a497d466893575c
Daniel Miguel <daniel.miguel@epita.fr>
2020-04-21 16:13:55 +0200

    NIKER VOUS SERIEUX

--- Player_related_stuff/Player_Control.cs
+++ Player_related_stuff/Player_Control.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Object = System.Object;
 
 public class Player_Control : MonoBehaviour
 {
@@ -12,12 +13,15 @@ public class Player_Control : MonoBehaviour
     private BoxCollider2D BoxCollider;
     private CircleCollider2D CircleCollider;
     [SerializeField] private LayerMask plateformLayerMask;
+    public GameObject Projectile;
+    public GameObject FirePoint;
     public float jump_speed = 10f;
     public float run_speed = 5f;
     public float gravity_modifier = 0.001f;
     private bool air_jump;
     private bool grounded;
     private string orientation;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -36,27 +40,7 @@ public class Player_Control : MonoBehaviour
             CircleCollider.bounds.extents.y + 0.05f , plateformLayerMask);
         grounded = hit.collider;
         // End of the test
-        
-        // Jump test
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x , jump_speed);
-            grounded = false;
-            air_jump = true;
-        }
-        
-        else if (Input.GetButtonDown("Jump") && air_jump)
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x , jump_speed);
-            air_jump = false;
-        }
 
-        if (Input.GetButtonUp("Jump") && rb2d.velocity.y > 0f)
-        {
-            rb2d.velocity = new Vector2(rb2d.velocity.x , 0f);
-        }
-        // End of jump test
-        
         // Computing ground normal and horizontal ground movement
         Vector2 ground_normal = hit.normal;
         Vector2 move_along = new Vector2(ground_normal.y , -ground_normal.x);
@@ -70,7 +54,7 @@ public class Player_Control : MonoBehaviour
                 if (orientation == "right")
                 {
                     orientation = "left";
-                    // Code flip du sprite
+                    transform.Rotate(0f, 180f, 0f);
                 }
                 
                 if (Input.GetKey(KeyCode.D))
@@ -86,7 +70,7 @@ public class Player_Control : MonoBehaviour
                 if (orientation == "left")
                 {
                     orientation = "right";
-                    // Code flip du sprite
+                    transform.Rotate (0f , 180f , 0f);
                 }
                 
                 if (Input.GetKey(KeyCode.Q))
@@ -121,31 +105,58 @@ public class Player_Control : MonoBehaviour
         }
         // End of Movement test
         
+        // Jump test
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb2d.velocity += Vector2.up * jump_speed;
+            grounded = false ;
+            air_jump = true;
+        }
+        
+        else if (Input.GetKeyDown(KeyCode.Space) && air_jump)
+        {
+            rb2d.velocity += Vector2.up * jump_speed;
+            air_jump = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && rb2d.velocity.y > 0f)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x , 0f);
+        }
+        // End of jump test
+        
         // Test of wall hit to prevent sticking to a wall
         RaycastHit2D left_bottom = Physics2D.Raycast(CircleCollider.bounds.center, Vector3.left,
-            CircleCollider.bounds.extents.x, plateformLayerMask);
+            CircleCollider.bounds.extents.x + 0.01f, plateformLayerMask);
         RaycastHit2D left_upper = Physics2D.Raycast(BoxCollider.bounds.center, Vector2.left,
             BoxCollider.bounds.extents.x + 0.01f, plateformLayerMask);
         if ((left_bottom.collider || left_upper.collider) && !grounded)
         {
             if (rb2d.velocity.x < 0)
             {
-                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+                rb2d.velocity -= Vector2.right * rb2d.velocity.x;
             }
         }
 
-        RaycastHit2D right_bottom = Physics2D.Raycast(CircleCollider.bounds.center, Vector2.right,
-            CircleCollider.bounds.extents.x, plateformLayerMask);
+        RaycastHit2D right_bottom = Physics2D.Raycast((CircleCollider.bounds.center), Vector2.right,
+            CircleCollider.bounds.extents.x + 0.01f, plateformLayerMask);
         RaycastHit2D right_upper = Physics2D.Raycast(BoxCollider.bounds.center, Vector2.right,
             BoxCollider.bounds.extents.x + 0.01f, plateformLayerMask);
         if ((right_bottom.collider || left_upper.collider) && !grounded)
         {
             if (rb2d.velocity.x > 0)
             {
-                rb2d.velocity = new Vector2(0 , rb2d.velocity.y);
+                rb2d.velocity -= Vector2.right * rb2d.velocity.x;
             }
         }
         // End Wall test
+        
+        // Fire Test
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Instantiate(Projectile, FirePoint.transform.position, FirePoint.transform.rotation);
+        }
+        // End fire test
     }
 
     private void FixedUpdate()
